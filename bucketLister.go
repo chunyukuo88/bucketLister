206b4db26ce51556630c51e5c98ee61026d38fd8
chunyukuo88 <alexgochenour@gmail.com>
2023-11-08 21:00:47 -0500

    Initialized project, created bucketLister.go file.

---
+++ bucketLister.go
@@ -0,0 +1,33 @@
+package listbuckets
+
+//begin import
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+var client *s3.client
+
+func init() {
+	config, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("Configuration error! It is: ", err.Error())
+	}
+	client = s3.NewFromConfig(config)
+}
+
+func ListBuckets() ([]*string, error) {
+	bucketArray := make([]*string, 0, 10)
+	params := &s3.ListBucketsInput{}
+	res, err := client.ListBuckets(context.TODO(), params)
+	if err != nil {
+		return nil, err
+	}
+	for _, bucket := range res.Buckets {
+		bucketArray = append(bucketArray, bucket.Name)
+	}
+
+	return bucketArray, nil
+}
